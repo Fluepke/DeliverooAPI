c84cc9842cd84bfb9eb6be918e972ddf2a534915
Fabian Lüpke <fabian.luepke@gmail.com>
2017-05-11 18:48:20 +0200

    New functionality added: Fetch menu items

--- src/main/java/one/rootz/deliverooapi/DeliverooDemo.java
+++ src/main/java/one/rootz/deliverooapi/DeliverooDemo.java
@@ -1,16 +1,32 @@
 package one.rootz.deliverooapi;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
 public class DeliverooDemo {
 
 	public static void main(String[] args) {
+		Boolean once = true;
 		System.out.println("Hello out there!");
 		Client client = new Client();
 		Session.begin(client);
-		for (Restaurant restaurant : Restaurant.getRestaurants(client, new Coordinate("52.5166791", "13.4584727"))) {
-			System.out.println(restaurant.toString());
+		try {
+			for (Restaurant restaurant : Restaurant.fetchRestaurants(client, new Coordinate("52.5166791", "13.4584727"))) {
+				System.out.println(restaurant.toString());
+				/*for (Restaurant.OpeningHours hours : restaurant.getOpeningHours()) {
+					System.out.println("\t" + hours.toString());
+				}*/
+				/*for (MenuTag menu : restaurant.getMenuTags()) {
+					System.out.println("\t" + menu.toString());
+				}*/
+				
+				if (once) {
+						restaurant.fetchMenuItems(client);
+						for (MenuItem menuItem : restaurant.getMenuItems()) {
+							System.out.println("\t" + menuItem.toString());
+						}
+					once = false;
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 
